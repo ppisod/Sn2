83d8360be6c1314c178ada50aacc7c5f72c1609b
ppisod <hicnkebromzih@gmail.com>
2025-11-08 18:46:13 +0800

    Changes: ppiLib/Display/Arrangement.cs ppiLib/Display/Transform.cs ppiLib/Types/_Attributes/Normalized.cs -> ppiLib/Types/_Attributes/Normalization.cs

---
+++ ppiLib/Display/Arrangement.cs
@@ -0,0 +1,11 @@
+﻿using ppiLib.Types;
+
+namespace ppiLib.Display;
+
+public class Arrangement
+{
+    public Q<V> Position { get; private set; }
+    public Q<V> Scale { get; private set; }
+    public Q<V> Rotation { get; private set; }
+    public Q<V> Padding { get; private set; }
+}
--- ppiLib/Display/Transform.cs
+++ ppiLib/Display/Transform.cs
@@ -1,6 +1,26 @@
-﻿namespace ppiLib.Display;
+﻿using ppiLib.Types;
+using ppiLib.Types._Attributes;
+
+namespace ppiLib.Display;
 
 public class Transform
 {
+    [Normalized]
+    public V LocalPosition { get; set; }
+    
+    [Normalized]
+    public V LocalScale { get; set; }
+    
+    [Normalized]
+    public V LocalRotation { get; set; }
+    
+    [Absolute]
+    public V Position { get; private set; }
+    
+    [Absolute]
+    public V Scale { get; private set; }
+    
+    [Absolute]
+    public V Rotation { get; private set; }
     
 }
