6644042911e79a7e5d50b3d5ae8e634d0421afd8
ppisod <hicnkebromzih@gmail.com>
2025-11-14 21:35:32 +0800

    Node.cs

--- ppiLib/Node/Node.cs
+++ ppiLib/Node/Node.cs
@@ -1,4 +1,5 @@
 #nullable enable
+using System.Collections.Generic;
 using ppiLib.Display;
 
 namespace ppiLib.Node;
@@ -6,17 +7,31 @@ namespace ppiLib.Node;
 /// <summary>
 /// A Node - a graphical object that can be added to the scene graph.
 /// Nodes can be transformed. But these transformations can be ignored.
-/// The Node system will take care of the transformations.
-/// The Node loves you.
+/// The Node parent-child system will take care of eeeverything.
 /// </summary>
-public abstract class Node ( NConfig config ) {
+public class Node ( NConfig config ) {
     
     public required Transform T { get; set; } = config.Transform;
     public Node? Parent { get; private set; } = config.Parent;
+    public List <Node> Children { get; private set; } = [];
 
-    public abstract void AddChild ( Node child );
-    public abstract void RemoveChild ( Node child );
-    public abstract void Draw ( );
-    public abstract void Update ( );
-    public abstract void Destroy ( );
+    public virtual void AddChild ( Node child ) {
+        
+    }
+
+    public virtual void RemoveChild ( Node child ) {
+        
+    }
+
+    public virtual void Draw ( ) {
+        
+    }
+
+    public virtual void Update ( ) {
+        
+    }
+
+    public virtual void Destroy ( ) {
+        
+    }
 }
